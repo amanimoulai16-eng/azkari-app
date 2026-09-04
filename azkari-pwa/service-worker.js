c91bfea11607f2b6c8f29bcfb33af4ac96ec76a3
amanimoulai16-eng <amanimoulai16@gmail.com>
2026-09-04 12:53:59 +0100

    Update service-worker.js

--- azkari-pwa/service-worker.js
+++ azkari-pwa/service-worker.js
@@ -1,8 +1,43 @@
-const CACHE_NAME = 'azkari-cache-v5';
-const ASSETS_TO_CACHE = [
+const CACHE_NAME = 'azkari-cache-v2';
+const FILES_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './azkari-icon-192.png',
-  './azkari-icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return Promise.all(
+        FILES_TO_CACHE.map((url) => cache.add(url).catch(() => {}))
+      );
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fetchPromise = fetch(event.request).then((response) => {
+        if (response && response.ok) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        }
+        return response;
+      }).catch(() => cached || caches.match('./index.html'));
+      return cached || fetchPromise;
+    })
+  );
+});
